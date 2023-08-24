4f470c475b61010c062bb09b59b226cd30376e3e
kajal <kajal@gmail.com>
2023-08-24 11:25:14 +0530

    ..

--- FileIO2.java
+++ FileIO2.java
@@ -5,16 +5,29 @@ public class FileIO2 {
     //persisent storage => files 
 
     public static void main(String[] args) throws IOException{
-        File ref  = new File("test.py");
+        File ref  = new File("javafolder");
 
-        if(ref.exists()){
-            System.out.println("the file exists");
-        }
-        else{
-            System.out.println("file does not exist");
-            ref.createNewFile();
+
+        System.out.println("file : "+ref.isFile());
+        System.out.println("folder : "+ref.isDirectory());
+
+
+        // if(ref.exists()){
+        //     System.out.println("it already exists");
+        // }
+        // else{
+        //     ref.mkdir();
+        // }
+
+        // if(ref.exists()){
+        //     System.out.println("the file exists");
+        // }
+        // else{
+        //     System.out.println("file does not exist");
+        //     ref.createNewFile();
             
-        }
+
+        // }
 
     }
 
